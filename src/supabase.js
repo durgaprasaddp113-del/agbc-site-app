a6b6c4581fe11d02e9fd4aade7e9f529661305d2
Tender <tender@alghaithconstruction.ae>
2026-05-01 14:35:12 +0400

    fix: hardcode supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  "https://awzxxzaspmwqgrywplnu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3enh4emFzcG13cWdyeXdwbG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NjQ3MDEsImV4cCI6MjAyOTU0MDcwMX0.Ql3IBVB7HuV1MBkG2Ys5yX0ZMoMlXJCB4MBiELyFTng"
+);
